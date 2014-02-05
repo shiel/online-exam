c6e1a2fdc82203f2a8543f4b325254549c31df5a
retsie <retsie@retsie-E412X.(none)>
2014-02-06 01:42:28 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,39 @@
+<<<<<<< HEAD
+<?php 
+$con =array(
+	'host' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'exam_db'
+	);
+$db =new mysqli(
+	$con['host'],
+	$con['username'],
+	$con['password'],
+	$con['database']
+	);
+if(mysqli_connect_errno()){
+	echo 'Could not connect to the database.';
+	exit;
+}
+ ?>
+=======
+<?php 
+$con =array(
+	'host' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'exam_db'
+	);
+$db =new mysqli(
+	$con['host'],
+	$con['username'],
+	$con['password'],
+	$con['database']
+	);
+if(mysqli_connect_errno()){
+	echo 'Could not connect to the database.';
+	exit;
+}
+ ?>
+>>>>>>> 076376c89e97622b0c11cbff781957b9399833c9
---
+++ logout.php
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+<?php 
+session_start();
+session_destroy();
+
+header('Location:loginpage.php');
+?>
+=======
+<?php 
+session_start();
+session_destroy();
+
+header('Location:loginpage.php');
+?>
+>>>>>>> 076376c89e97622b0c11cbff781957b9399833c9
